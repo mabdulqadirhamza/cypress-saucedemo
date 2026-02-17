8600c1e234968e0e3d08fb4478ac182ae3778d7b
abdulqadir5679 <aq2025b5@gmail.com>
2026-02-17 18:27:37 +0500

    Checkout page added

---
+++ cypress/pages/CheckoutPage.js
@@ -0,0 +1,31 @@
+class CheckoutPage {
+    
+    //selectors
+    elements = {
+        firstNameInput:  () => cy.get('[data-test="firstName"]'),
+        lastNameInput:   () => cy.get('[data-test="lastName"]'),
+        postalCodeInput: () => cy.get('[data-test="postalCode"]'),
+        continueButton:  () => cy.get('[data-test="continue"]'),
+        finishButton:    () => cy.get('[data-test="finish"]'),
+        successHeader:   () => cy.get('[data-test="complete-header"]'),
+    }
+
+    //actions
+    fillCustomerInfo(firstName, lastName, postalCode){
+        this.elements.firstNameInput().type(firstName)
+        this.elements.lastNameInput().type(lastName)
+        this.elements.postalCodeInput().type(postalCode)
+        this.elements.continueButton().click()
+    }
+
+    finishOrder(){
+        this.elements.finishButton().click()
+    }
+
+    //assertions
+    verifyOrderComplete(){
+        this.elements.successHeader().should('be.visible').and('contain', 'THANK YOU FOR YOUR ORDER')
+    }
+}
+
+export default CheckoutPage
